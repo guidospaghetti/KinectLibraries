8b2ae001fd6b734805b4ea32953da548a94e5691
Aaron Guidarelli <guidarela2@students.rowan.edu>
2017-07-30 12:46:30 -0400

    Modify testing file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include "KinectLibrary.h"
-
+#include <opencv2\highgui.hpp>
 int main() {
 	
-	KinectLibrary kl(COLOR_SENSOR | DEPTH_SENSOR);
-	cv::Mat color, depth;
-	kl.getColorImage(color);
-	kl.getDepthImage(depth);
-
+	KinectLibrary kl(COLOR_SENSOR | BODY_INDEX_SENSOR);
+	cv::Mat color, bodyIndex;
+	while (1) {
+		if (kl.getColorImage(color)) {
+			cv::imshow("Image", color);
+			cv::waitKey(1);
+		}
+		//if (kl.getBodyIndex(bodyIndex)) {
+		//	cv::imshow("BodyIndex", bodyIndex);
+		//	cv::waitKey(1);
+		//}
+	}
 	kl.~KinectLibrary();
+
 	return 0;
 }
