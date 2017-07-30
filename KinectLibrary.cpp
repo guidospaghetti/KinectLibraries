7def4bd8007e85a0bd804d30852752febd459eab
Aaron Guidarelli <guidarela2@students.rowan.edu>
2017-07-29 20:40:07 -0400

    Complete constructor

--- KinectLibrary.cpp
+++ KinectLibrary.cpp
@@ -1,12 +1,14 @@
 #include "KinectLibrary.h"
 #include "KinectLibraryException.h"
 #include "Windows.h"
-#include "Kinect.h"
+
 #include "Ole2.h"
-#include <stdint.h>
+
 #include <intrin.h>
 
 KinectLibrary::KinectLibrary(uint8_t sensors) {
+
+	_sensors = sensors;
 
 	HRESULT result = GetDefaultKinectSensor(&sensor);
 	int count = 0;
@@ -17,6 +19,11 @@ KinectLibrary::KinectLibrary(uint8_t sensors) {
 		return;
 	}
 	
+	result = sensor->Open();
+	if (FAILED(result)) {
+		throw KinectLibraryException("Failed to open the Kinect sensor");
+	}
+
 	if (__popcnt16((uint16_t)sensors) > 1) {
 		DWORD frameSourceTypes = 0;
 		if (sensors & COLOR_SENSOR) {
@@ -33,6 +40,7 @@ KinectLibrary::KinectLibrary(uint8_t sensors) {
 		}
 
 		result = sensor->OpenMultiSourceFrameReader((DWORD)frameSourceTypes, &msfReader);
+
 		if (FAILED(result)) {
 			throw KinectLibraryException("Failed to open the multi-source frame reader");
 		}
@@ -40,20 +48,114 @@ KinectLibrary::KinectLibrary(uint8_t sensors) {
 	else {
 		switch (sensors) {
 		case COLOR_SENSOR:
+
+			initColor();
+
 			break;
 		case DEPTH_SENSOR:
+
+			initDepth();
+
 			break;
 		case INFRARED_SENSOR:
+
+			initInfrared();
+
 			break;
 		case BODY_SENSOR:
+
+			initBody();
+
 			break;
 		default:
 			break;
 		}
+		
+		if (frameDesc) {
+			frameDesc->get_Height(&height);
+			frameDesc->get_Width(&width);
+		}
+
 	}
 
 }
 
+void KinectLibrary::initColor() {
+
+	HRESULT result = sensor->get_ColorFrameSource(&colorSource);
+
+	if (FAILED(result)) {
+		throw KinectLibraryException("Unable to get color source");
+	}
+
+	result = colorSource->OpenReader(&colorReader);
+
+	if (FAILED(result)) {
+		throw KinectLibraryException("Unable to get color reader");
+	}
+
+	result = colorSource->get_FrameDescription(&frameDesc);
+
+	if (FAILED(result)) {
+		throw KinectLibraryException("Unable to get frame description");
+	}
+}
+
+void KinectLibrary::initDepth() {
+	HRESULT result = sensor->get_DepthFrameSource(&depthSource);
+
+	if (FAILED(result)) {
+		throw KinectLibraryException("Unable to get depth source");
+	}
+
+	result = depthSource->OpenReader(&depthReader);
+
+	if (FAILED(result)) {
+		throw KinectLibraryException("Unable to get depth reader");
+	}
+
+	result = depthSource->get_FrameDescription(&frameDesc);
+
+	if (FAILED(result)) {
+		throw KinectLibraryException("Unable to get frame description");
+	}
+}
+
+void KinectLibrary::initInfrared() {
+	HRESULT result = sensor->get_InfraredFrameSource(&infraredSource);
+
+	if (FAILED(result)) {
+		throw KinectLibraryException("Unable to get infrared source");
+	}
+
+	result = infraredSource->OpenReader(&infraredReader);
+
+	if (FAILED(result)) {
+		throw KinectLibraryException("Unable to get infrared reader");
+	}
+
+	result = infraredSource->get_FrameDescription(&frameDesc);
+
+	if (FAILED(result)) {
+		throw KinectLibraryException("Unable to get frame description");
+	}
+
+}
+
+void KinectLibrary::initBody() {
+	HRESULT result = sensor->get_BodyFrameSource(&bodySource);
+
+	if (FAILED(result)) {
+		throw KinectLibraryException("Unable to get body source");
+	}
+
+	result = bodySource->OpenReader(&bodyReader);
+
+	if (FAILED(result)) {
+		throw KinectLibraryException("Unable to get body reader");
+	}
+}
 
 KinectLibrary::~KinectLibrary() {
 }
+
--- KinectLibrary.h
+++ KinectLibrary.h
@@ -1,3 +1,6 @@
+#include "Kinect.h"
+#include <stdint.h>
+#include <opencv2\core\core.hpp>
 
 #define	COLOR_SENSOR		0x01
 #define DEPTH_SENSOR		0x01 << 1
@@ -7,16 +10,76 @@
 class KinectLibrary {
 
 public:
-
 	KinectLibrary(uint8_t sensors);
 
 	~KinectLibrary();
 
 private:
 
+	uint8_t _sensors;
+
 	IKinectSensor* sensor;
 
 	IMultiSourceFrameReader* msfReader;
 
+	IMultiSourceFrame* msf;
+
+	IColorFrameSource* colorSource;
+
+	IColorFrameReader* colorReader;
+
+	IColorFrameReference* colorRef;
+
+	IColorFrame* colorFrame;
+
+	IDepthFrameSource* depthSource;
+	
+	IDepthFrameReader* depthReader;
+
+	IDepthFrameReference* depthRef;
+
+	IDepthFrame* depthFrame;
+
+	IInfraredFrameSource* infraredSource;
+
+	IInfraredFrameReader* infraredReader;
+
+	IInfraredFrameReference* infraredRef;
+
+	IInfraredFrame* infraredFrame;
+
+	IBodyFrameSource* bodySource;
+	
+	IBodyFrameReader* bodyReader;
+
+	IBodyFrameReference* bodyRef;
+
+	IBodyFrame* bodyFrame;
+
+	IFrameDescription* frameDesc;
+
+	int colorHeight;
+
+	int colorWidth;
+	
+	int depthHeight;
+	
+	int depthWidth;
+	
+	int infraredHeight;
+	
+	int infraredWidth;
+
+	int height;
+
+	int width;
+
+	void initColor();
+
+	void initDepth();
+
+	void initInfrared();
+
+	void initBody();
 };
 
